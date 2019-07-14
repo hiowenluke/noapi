07bdb48b0b0bf2d4c8fc797714b67d71074c25a4
Owen Luke <hi.owen.luke@gmail.com>
2019-07-14 17:09:23 +0800

    fix options with default values

--- src/index.js
+++ src/index.js
@@ -2,7 +2,14 @@
 const routes = require('./routes');
 const web = require('./web');
 
-const noapi = (options) => {
+const optionsDefault = {
+	serverName: 'default',
+	port: 3000,
+};
+
+const noapi = (options = {}) => {
+	options.serverName = options.serverName || optionsDefault.serverName;
+	options.port = options.port || optionsDefault.port;
 	return web(options, routes);
 };
 
