b0be950b04f529290c42387c1076f6fc5a265596
Owen Luke <hi.owen.luke@gmail.com>
2019-07-14 23:02:56 +0800

    If there is no index.js in the aha, api, and biz directories, simulate it

--- src/routes/init/loadApiServices.js
+++ src/routes/init/loadApiServices.js
@@ -1,6 +1,7 @@
 
 const fs = require('fs');
 const path = require('path');
+const kdo = require('kdo');
 const data = require('../../data');
 
 // The serviceName is the api service directory name, such as "api", "api-forms", etc.
@@ -13,8 +14,28 @@ const initServiceInfo = (serviceName) => {
 
 	coreModuleNames.forEach(coreModuleName => {
 		const coreFolderPath = path.resolve(folderPath + '/' + coreModuleName);
+
+		// There is .../aha or .../api or .../biz directory
 		if (fs.existsSync(coreFolderPath)) {
-			coreModules[coreModuleName] = require(coreFolderPath);
+
+			if (fs.existsSync(coreFolderPath + '/index.js')) {
+				coreModules[coreModuleName] = require(coreFolderPath);
+			}
+			else {
+				// But there is no .../index.js
+
+				// Simulate index.js as module.filename for kdo.
+				// Note it is required for flag isSimulatedIndexJs.
+				const module = {filename: coreFolderPath + '/index.js', isSimulatedIndexJs: true};
+
+				if (coreModuleName === 'aha') {
+					coreModules[coreModuleName] = kdo.flow(module, 'query');
+				}
+
+				if (coreModuleName === 'api' || coreModuleName === 'biz') {
+					coreModules[coreModuleName] = kdo.obj(module);
+				}
+			}
 		}
 	});
 
