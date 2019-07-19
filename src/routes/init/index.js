71a584849321571a5852927f2a8ff3328a76e2c4
Owen Luke <hi.owen.luke@gmail.com>
2019-07-19 16:23:13 +0800

    simplify the code in src/routes/init/index.js

--- src/routes/init/index.js
+++ src/routes/init/index.js
@@ -1,11 +1,2 @@
 
-const me = require('kdo').obj(module);
-
-const fn = (options) => {
-
-	me.initWebServiceRoot(options.module);
-	me.initApiServices(options.apiPath);
-	me.loadApiServices();
-};
-
-module.exports = fn;
+module.exports = require('kdo').flow(module, 'options');
