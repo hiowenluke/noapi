a351679f92adad578acfe78ebe96dabae1f9716e
Owen Luke <hi.owen.luke@gmail.com>
2019-07-19 17:59:19 +0800

    src/routes/init/index.js: use kdo.sync.flow instead fo kdo.flow

--- src/routes/init/index.js
+++ src/routes/init/index.js
@@ -1,2 +1,4 @@
 
-module.exports = require('kdo').flow(module, 'options');
+// They are all synchronous functions in this directory,
+// so we need to use kdo.sync.flow instead fo kdo.flow.
+module.exports = require('kdo').sync.flow(module, 'options');
