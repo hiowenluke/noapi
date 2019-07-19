24dd7660659d5ef80ac8425d1680c7d25e0dd6df
Owen Luke <hi.owen.luke@gmail.com>
2019-07-19 16:13:12 +0800

    move data.assignRules to src/index.js

--- src/routes/init/index.js
+++ src/routes/init/index.js
@@ -1,14 +1,11 @@
 
 const me = require('kdo').obj(module);
-const data = require('../../data');
 
 const fn = (options) => {
 
 	me.initWebServiceRoot(options.module);
 	me.initApiServices(options.apiPath);
 	me.loadApiServices();
-
-	data.assignRules = options.assignRules;
 };
 
 module.exports = fn;
