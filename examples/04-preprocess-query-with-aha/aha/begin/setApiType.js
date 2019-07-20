1a612ff43313d2bdac8101454fb66fe70f210d9d
Owen Luke <hi.owen.luke@gmail.com>
2019-07-20 20:39:38 +0800

    .

--- examples/04-preprocess-query-with-aha/aha/begin/setApiType.js
+++ examples/04-preprocess-query-with-aha/aha/begin/setApiType.js
@@ -6,8 +6,6 @@ const fn = ({query}) => {
 	// Use noapi lib to check if it is a assigned path
 	query.isBill = noapi.url.isStartWith(query, '/bill');
 	query.isInfo = noapi.url.isStartWith(query, '/info');
-
-	console.log(query)
 };
 
 module.exports = fn;
