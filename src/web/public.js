df16573f98f5dc8d6880366b44457b77f0299f1a
Owen Luke <hi.owen.luke@gmail.com>
2019-07-22 16:06:02 +0800

    move code of public in src/web/index.js to public.js

---
+++ src/web/public.js
@@ -0,0 +1,32 @@
+
+const me = {
+	init(options, express, expressApp) {
+		if (!options.public) return;
+
+		if (typeof options.public === 'object') {
+
+			// options.public = {path}, without name
+			if (!options.public.name) {
+				options.public.name = 'public';
+			}
+
+			// Use the root folder of project as public folder
+			if (options.public.name === '/') {
+				options.public.name = '';
+			}
+		}
+
+		// If options.public is string, it is a path to public
+		if (typeof options.public === 'string') {
+			const pathToPublic = options.public;
+			options.public = {
+				name: 'public',
+				path: pathToPublic,
+			}
+		}
+
+		expressApp.use('/' + (options.public.name || ''), express.static(options.public.path));
+	}
+};
+
+module.exports = me;
