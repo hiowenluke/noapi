6971083f0464d7d9407ac6828b45d6e1dd3a40c0
Owen Luke <hi.owen.luke@gmail.com>
2019-07-22 14:06:52 +0800

    .

--- src/routes/init/index.js
+++ src/routes/init/index.js
@@ -1,4 +1,4 @@
 
-// They are all synchronous functions in this directory,
+// They are synchronous functions in this directory,
 // so we need to use kdo.sync.flow instead fo kdo.flow.
 module.exports = require('kdo').sync.flow(module, 'options');
