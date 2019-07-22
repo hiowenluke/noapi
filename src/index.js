71612e3326608bd60aaa6977914a6afd894299ac
Owen Luke <hi.owen.luke@gmail.com>
2019-07-22 16:09:00 +0800

    move the code of data in src/index.js to src/data.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
 
-const fs = require('fs');
-const path = require('path');
 const caller = require('caller');
 
 const data = require('./__data');
@@ -8,41 +6,9 @@ const routes = require('./routes');
 const utils = require('./utils');
 const web = require('./web');
 
-const optionsDefault = {
-	serverName: 'default',
-	port: 3000,
-};
-
-const getWebServiceRoot = (pathToCaller) => {
-
-	if (pathToCaller === '/') {
-		throw new Error('no package.json found in parent path of ' + pathToCaller);
-	}
-
-	// Find package.json in parent path
-	const parentPath = path.resolve(pathToCaller, '..');
-	const packageJson = parentPath + '/package.json';
-
-	// Found the package.json
-	if (fs.existsSync(packageJson)) {
-
-		// The parent path is the web services root directory
-		return parentPath;
-	}
-	else {
-		// Not found
-		// Recurse to Find
-		return getWebServiceRoot(parentPath);
-	}
-};
 
 const noapi = (options = {}) => {
 
-	data.webServiceRoot = getWebServiceRoot(caller());
-	data.assignRules = options.assignRules;
-
-	options.serverName = options.serverName || optionsDefault.serverName;
-	options.port = options.port || optionsDefault.port;
 
 	return web(options, routes);
 };
