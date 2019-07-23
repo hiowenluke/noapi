786bb96f654b6922a2efe24ab1e93fdf360a7de5
Owen Luke <hi.owen.luke@gmail.com>
2019-07-23 13:37:39 +0800

    define/load.js: merge new apis to data.core[sysName].api

--- src/api/define/load.js
+++ src/api/define/load.js
@@ -1,4 +1,5 @@
 
+const _ = require('lodash');
 const data = require('../../data');
 const lib = require('../../__lib');
 const parse = require('./parse');
@@ -11,7 +12,11 @@ const fn = () => {
 	const apis = apiInfos.map(item => item.api);
 	apis.forEach(api => { // /bill/form/crud
 		const {sysName, apiPath} = lib.apiParser.parseApiUrlToSysNameAndApiPath(api);
-		data.core[sysName].api = lib.apiParser.parseApiPathToObject(apiPath);
+		const apis = lib.apiParser.parseApiPathToObject(apiPath);
+
+		const core = data.core[sysName];
+		if (!core.api) core.api = {};
+		core.api = _.merge(core.api, apis);
 	});
 };
 
