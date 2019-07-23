ff981f454b77ddf3ea1e296aa10452622d2a2c2d
Owen Luke <hi.owen.luke@gmail.com>
2019-07-23 11:34:39 +0800

    parse.js: done

--- src/api/define/parse.js
+++ src/api/define/parse.js
@@ -30,31 +30,77 @@ const parser = {
 		const api = this.getApiFromUrl(url);
 		const title = this.getTitleFromApi(api);
 		return {api, title};
+	},
+
+	getUrlFromApi(api) {
+		const {http, port} = data.serverOptions;
+		const portStr = port ? ':' + port : '';
+		const url = `http://${http}${portStr}${api}`;
+		return url;
+	},
+
+	getApiTitleUrlFromString(str) {
+		let api, title, url;
+
+		if (str.substr(0, 1) === '/') { // /bill/form/crud
+			api = str;
+			title = this.getTitleFromApi(api);
+			url = this.getUrlFromApi(api);
+		}
+		else {
+			// http://localhost:3000/bill/form/crud?formname=trader
+			url = str;
+			({api, title} = this.getApiTitleFromUrl(url));
+		}
+
+		return {api, title, url};
 	}
 };
 
 /** @name define.parse */
-const fn = () => {
-	if (data.apiDefineJsPaths.length === 0) return;
+const me = {
+	forApiInfos() {
+		if (data.apiDefineJsPaths.length === 0) return;
 
-	const defs = [];
-	data.apiDefineJsPaths.forEach(defineJsPath => {
-		const apiDefineArr = require(defineJsPath);
-		apiDefineArr.forEach(item => {
+		const apiInfos = [];
+		data.apiDefineJsPaths.forEach(defineJsPath => {
+			const apiDefineArr = require(defineJsPath);
 
-			// 'http://localhost:3000/bill/form/crud?formname=trader'
-			if (typeof item === 'string') {
-				const url = item;
-				const {api, title} = parser.getApiTitleFromUrl(url);
-				const def = {api, title, url};
-				defs.push(def);
-			}
+			apiDefineArr.forEach(item => {
 
-			if (_.isPlainObject(item)) {
+				// api: /bill/form/crud
+				// url: http://localhost:3000/bill/form/crud?formname=trader
+				if (typeof item === 'string') {
+					const {api, title, url} = parser.getApiTitleUrlFromString(item);
+					apiInfos.push({api, title, url});
+				}
 
-			}
+				// {api, title, url}
+				if (_.isPlainObject(item)) {
+					let {api, title, url} = item;
+
+					// There must be at least one api and url.
+					if (!api && !url) return;
+
+					if (!api) { // There is a url
+						api = parser.getApiFromUrl(url);
+					}
+					else { // There is a api
+						url = parser.getUrlFromApi(api);
+					}
+
+					title = parser.getTitleFromApi(api);
+					apiInfos.push({api, title, url});
+				}
+			});
 		});
-	});
+
+		return apiInfos;
+	},
+
+	forAll() {
+
+	}
 };
 
-module.exports = fn;
+module.exports = me;
