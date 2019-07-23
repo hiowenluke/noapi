02dbb7c0197ed41b746b9cd7f2a5ae07a5ffb306
Owen Luke <hi.owen.luke@gmail.com>
2019-07-23 16:12:53 +0800

    examples: remove 03-custom-api-function

--- examples/03-custom-api-function/biz/bill/form/crud.js
+++
@@ -1,7 +0,0 @@
-
-const fn = async (query) => {
-	let requestTime = query.isReturnRequestTime ? new Date().getTime() : null;
-	return {formname: query.formname, requestTime};
-};
-
-module.exports = fn;
