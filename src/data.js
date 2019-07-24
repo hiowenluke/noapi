0c7e77e2ac36016fc43ec0aeff2eeab6a91e4ae3
Owen Luke <hi.owen.luke@gmail.com>
2019-07-24 14:32:42 +0800

    .

--- src/data.js
+++ src/data.js
@@ -17,65 +17,7 @@ const core = {
 				form: {	//				/form
 					crud: {}, //			/crud.js
 				}
-			},
-
-			__define__: { // /<user project>/api/define.js // refer /noapi/src/api/define/__model.js
-
-				api: {
-					api: '/bill/form/crud', // If omitted, parse from url
-					title: 'Bill - Form - Crud', // If omitted, parse from api
-					url: 'http://localhost:3000/bill/form/crud?formname=trader',
-				},
-
-				// io: input params, output result
-				io: {
-
-					// Send it to server. If omitted, parse from url
-					params: {
-						formname: 'trader',
-					},
-
-					// Get it from server. It can be omitted if not required for testing.
-					result: {
-						"success": true,
-						"data": {
-							"formname": "trader"
-						}
-					},
-				},
-
-				test: {
-
-					// Call specific apis before do with test url if needed.
-					// E.g., insert some data to db before do with test url.
-					// The beforeDo can be an array, or an api, title, url, or some other specified property.
-					beforeDo: [
-						'/bill/form/crud', // by api
-						'Bill - Form - Crud', // by title
-						'http://localhost:3000/bill/form/crud?formname=trader', // by url
-						'id@123', // by some other specified property, such as id, e.g., {id: 123, api: '/xxx', ...}
-					],
-
-					// The test url. If omitted, use the demo url.
-					// E.g., the test url carries more parameters than the demo url for specific purposes.
-					url: undefined,
-
-					// How to get the result. If omitted, use the demo url.
-					// E.g., after deleting the data via test url, re-acquire the data to verify if it is exists.
-					// The usage is the same as beforeDo.
-					getResult: undefined,
-
-					// Call specific apis after get the test result if needed.
-					// E.g., delete the inserted data in before.
-					// The usage is the same as beforeDo.
-					afterDo: undefined,
-
-					// See above section "4. With test ..." for the usage of verify
-					verify(resultText, resultObject) {
-						return resultText.indexOf(`"formname":"trader"`) >= 0;
-					}
-				}
-			},
+			}
 		},
 
 		biz: { // /<user project>/biz
@@ -104,6 +46,66 @@ const core = {
 	},
 };
 
+// The demo of data.defineJs
+const defineJs = {
+	filename: "", // // /<user project>/api/define.js // refer /noapi/src/api/define/__model.js
+
+	api: {
+		api: '/bill/form/crud', // If omitted, parse from url
+		title: 'Bill - Form - Crud', // If omitted, parse from api
+		url: 'http://localhost:3000/bill/form/crud?formname=trader',
+	},
+
+	// io: input params, output result
+	io: {
+
+		// Send it to server. If omitted, parse from url
+		params: {
+			formname: 'trader',
+		},
+
+		// Get it from server. It can be omitted if not required for testing.
+		result: {
+			"success": true,
+			"data": {
+				"formname": "trader"
+			}
+		},
+	},
+
+	test: {
+
+		// Call specific apis before do with test url if needed.
+		// E.g., insert some data to db before do with test url.
+		// The beforeDo can be an array, or an api, title, url, or some other specified property.
+		beforeDo: [
+			'/bill/form/crud', // by api
+			'Bill - Form - Crud', // by title
+			'http://localhost:3000/bill/form/crud?formname=trader', // by url
+			'id@123', // by some other specified property, such as id, e.g., {id: 123, api: '/xxx', ...}
+		],
+
+		// The test url. If omitted, use the demo url.
+		// E.g., the test url carries more parameters than the demo url for specific purposes.
+		url: undefined,
+
+		// How to get the result. If omitted, use the demo url.
+		// E.g., after deleting the data via test url, re-acquire the data to verify if it is exists.
+		// The usage is the same as beforeDo.
+		getResult: undefined,
+
+		// Call specific apis after get the test result if needed.
+		// E.g., delete the inserted data in before.
+		// The usage is the same as beforeDo.
+		afterDo: undefined,
+
+		// See above section "4. With test ..." for the usage of verify
+		verify(resultText, resultObject) {
+			return resultText.indexOf(`"formname":"trader"`) >= 0;
+		}
+	}
+};
+
 /** @name me.data */
 const me = {
 	webServiceRoot: '', // root path of web service
@@ -122,7 +124,7 @@ const me = {
 	serviceSysNames: {}, // {"api-forms": "forms", "api-erp": "erp", "api-mms": "mms"} // for getting sysName by serviceName
 
 	core: {}, // {aha, api, biz} // see the demo of it at top of this file
-	defineJs: {}, // {"default": "/path/to/user project/api/define.js"}
+	defineJs: {}, // {default: {filename, api, io, test}} // see the demo of it at top of this file
 
 	assignRules: [], // rules of assigning
 
