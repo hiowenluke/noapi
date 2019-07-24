f3831698fa5c82bcb36430e02dbf6567de9a665d
Owen Luke <hi.owen.luke@gmail.com>
2019-07-24 13:51:46 +0800

    add the demo for data.core

--- src/data.js
+++ src/data.js
@@ -2,6 +2,80 @@
 const fs = require('fs');
 const path = require('path');
 
+// The demo of data.core
+const core = {
+	default: {
+
+		// A function
+		// Execute all function files in the aha directory in the specified order with kdo.
+		aha: { // /<user project>/aha
+
+		},
+
+		api: { // /<user project>/api
+			bill: { //				/bill
+				form: {	//				/form
+					crud: {}, //			/crud.js
+				}
+			},
+
+			__define__: { // /<user project>/api/define.js // refer /noapi/src/api/define/__model.js
+
+				apiInfos: {
+					api: '/bill/form/crud', // If omitted, parse from url
+					title: 'Bill - Form - Crud', // If omitted, parse from api
+					url: 'http://localhost:3000/bill/form/crud?formname=trader',
+				},
+
+				// io: input params, output result
+				ioInfos: {
+
+					// Send it to server. If omitted, parse from url
+					params: {
+						formname: 'trader',
+					},
+
+					// Get it from server. It can be omitted if not required for testing.
+					result: {
+						"success": true,
+						"data": {
+							"formname": "trader"
+						}
+					},
+				},
+
+				testInfos: {
+
+				}
+			},
+		},
+
+		biz: { // /<user project>/biz
+			bill: { // 				/bill
+				form: { //  			/form
+					crud: {}, // 			/crud.js
+				}
+			}
+		}
+	},
+
+	api: { // The sysName is "api"
+		// ...
+	},
+
+	forms: { // The sysName is "forms"
+		// ...
+	},
+
+	erp: { // The sysName is "erp"
+		// ...
+	},
+
+	mms: { // The sysName is "mms"
+		// ...
+	},
+};
+
 /** @name me.data */
 const me = {
 	webServiceRoot: '', // root path of web service
