40eac9650afe458ad465401fc56c8104baf9ed63
Owen Luke <hi.owen.luke@gmail.com>
2019-07-24 18:48:41 +0800

    define/parseDefineJs.js: update forTest()

--- src/api/define/parseDefineJs.js
+++ src/api/define/parseDefineJs.js
@@ -4,6 +4,56 @@ const v = require('voca');
 const data = require('../../data');
 const lib = require('../../__lib');
 
+const getVerifyFn = {
+	forMatchingResultExactly(item) {
+		return (resultText, result) => {
+			let comparisonResult = item.result;
+			let comparisonFile = item.resultComparisonFile;
+
+			// If the target is a comparison file:
+			// 		"bill.form.crud" => "/path/to/user project/test/comparison/bill.form.crud.json"
+			if (typeof comparisonResult === 'string' || comparisonFile) {
+				const filename = comparisonResult || comparisonFile;
+				const testRoot = data.testRoot;
+
+				// Require the comparison file
+				comparisonResult = require(testRoot + `/comparison/${filename}.json`);
+			}
+
+			// Both must match exactly
+			return _.isEqual(result, comparisonResult);
+		}
+	},
+
+	forContainingKeyValues(expectedData) {
+		return (resultText, result) => {
+			let isOK = true;
+
+			// Result must contains the property value in expectedData
+			Object.keys(expectedData).find(key => {
+				if (!_.isEqual(expectedData[key], result[key])) {
+					isOK = false;
+					return key;
+				}
+			});
+
+			return isOK;
+		}
+	},
+
+	forContainingString(expectedString) {
+		return (resultText, result) => {
+			return resultText.indexOf(expectedString) >= 0;
+		}
+	},
+
+	forRegExp(reg) {
+		return (resultText, result) => {
+			return reg.test(resultText);
+		}
+	}
+};
+
 const parseDefineJs = {
 	do(type) {
 		const method = 'for' + v.titleCase(type); // apiInfos => forApiInfos
@@ -74,8 +124,113 @@ const parseDefineJs = {
 		apiDefineArr.forEach(item => {
 			if (!_.isPlainObject(item)) return;
 
-			let {params, result} = item;
-			testInfos.push({params, result});
+			let test;
+
+			// If the test property is omitted
+			if (!item.test) {
+
+				// The result returned from server must be matches the result property exactly
+				if (item.result || item.resultComparisonFile) {
+					test = {
+						verify: getVerifyFn.forMatchingResultExactly(item)
+					};
+				}
+			}
+			else {
+				// There is a test property
+				test = item.test;
+
+				// test: {
+				// 		...
+				// }
+				if (_.isPlainObject(test)) {
+
+					// A simple object:
+					// 		test: {
+					// 				formname: "trader",
+					//				// verify, // without verify
+					// 		}
+					if (!test.verify) {
+						test = {
+							verify: getVerifyFn.forContainingKeyValues(test)
+						}
+					}
+					else {
+						// A standard test object:
+						// 		test: {
+						// 				beforeDo,
+						// 				url,
+						// 				getResult,
+						// 				afterDo,
+						// 				verify, // with verify (multiple forms)
+						// 		}
+
+						// verify(resultText, result) {
+						// 		...
+						// }
+						if (typeof test.verify === 'function') {
+							// do nothing
+						}
+
+						// verify: {
+						// 		formname: 'trader',
+						// }
+						if (_.isPlainObject(test.verify)) {
+							test.verify = getVerifyFn.forContainingKeyValues(test.verify);
+						}
+
+						// verify:
+						// 		`"formname":"trader"`,
+						if (typeof test.verify === 'string') {
+							test.verify = getVerifyFn.forContainingString(test.verify);
+						}
+
+						// verify:
+						// 		/(traderid)|(goodsid)/,
+						if (test.verify instanceof RegExp) {
+							test.verify = getVerifyFn.forRegExp(test.verify);
+						}
+					}
+				}
+				else {
+
+					// test(resultText, result) {
+					// 		...
+					// }
+					if (typeof test === 'function') {
+						test = {
+							verify: test
+						}
+					}
+
+					// test: {
+					// 		formname: 'trader',
+					// }
+					if (_.isPlainObject(test)) {
+						test = {
+							verify: getVerifyFn.forContainingKeyValues(test)
+						}
+					}
+
+					// test:
+					// 		`"formname":"trader"`,
+					if (typeof test === 'string') {
+						test = {
+							verify: getVerifyFn.forContainingString(test)
+						};
+					}
+
+					// test:
+					// 		/(traderid)|(goodsid)/,
+					if (test instanceof RegExp) {
+						test = {
+							verify: getVerifyFn.forRegExp(test)
+						};
+					}
+				}
+			}
+
+			testInfos.push(test);
 		});
 
 		return testInfos;
