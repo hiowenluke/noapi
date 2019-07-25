0795251d7886d70179874e0b0d2cb14060a46743
Owen Luke <hi.owen.luke@gmail.com>
2019-07-26 00:04:44 +0800

    src/test/index.js: move getApiUrlByTypeStr() to lib.getApiUrlByTypeStr()

--- src/test/index.js
+++ src/test/index.js
@@ -8,38 +8,7 @@ const request = require('./request');
 
 const data = require('../data');
 const apiInit = require('../api/init');
-
-// types:
-// 		'/bill/form/crud', // by api
-// 		'Bill - Form - Crud', // by title
-// 		'http://localhost:3000/bill/form/crud?formname=trader', // by url
-const getApiUrlByTypeStr = (typeStr) => {
-
-	// If the typeStr is a url, return it directly
-	if (/^http[s]*:/i.test(typeStr)) { // url
-		return typeStr;
-	}
-
-	let apiUrl;
-
-	data.sysNames.find(sysName => {
-		const defineJs = data.defineJs[sysName];
-		const {api} = defineJs;
-
-		const item = api.find(apiInfo => {
-			const {api, title, url} = apiInfo;
-
-			if (typeStr === api || typeStr === title) {
-				apiUrl = url;
-				return true;
-			}
-		});
-
-		if (!!item) return true;
-	});
-
-	return apiUrl;
-};
+const lib = require('./__lib');
 
 const flow = {
 	initData({pathToCaller}) {
