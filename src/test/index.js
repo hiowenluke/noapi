992e3a27e272f4898d9986d9b861d26e61c1a367
Owen Luke <hi.owen.luke@gmail.com>
2019-07-26 13:42:32 +0800

    move createDescribes() to src/test/createDescribes/index.js

--- src/test/index.js
+++ src/test/index.js
@@ -37,48 +37,10 @@ const flow = {
 		});
 	},
 
-	createDescribes() {
-		const serviceNames = data.serviceNames;
 
-		serviceNames.forEach(serviceName => {
-			const title = serviceName === 'default' ? 'api' : serviceName;
 
-			describe(title, () => {
-				const sysName = data.serviceSysNames[serviceName];
-				const defineJs = data.defineJs[sysName];
-				const {api, docs} = defineJs;
 
-				for (let i = 0; i < api.length; i ++) {
-					const apiInfo = api[i];
-					const docInfos = docs[i];
 
-					if (!config.isValidTestCase(apiInfo.title)) {
-						continue;
-					}
-
-					if (!docInfos.length) {
-						it('- No test', () => {});
-						continue;
-					}
-
-					docInfos.forEach(docInfo => {
-						const ioInfo = docInfo.io;
-						const testInfo = docInfo.test;
-
-						// No testInfo property and result property, no need to test
-						if (!testInfo && !ioInfo.result) return;
-
-						// No verify property, can not test
-						if (!testInfo.verify) return;
-
-						testInfo.testUrl = testInfo.url || apiInfo.url;
-						testInfo.getResult = testInfo.getResult || apiInfo.url;
-
-						createTestIt(apiInfo, ioInfo, testInfo);
-					});
-				}
-			});
-		});
 	}
 };
 
