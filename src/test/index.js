0ba993e38b43e35eddaf5da896e160dfc8c1b005
Owen Luke <hi.owen.luke@gmail.com>
2019-07-26 13:44:34 +0800

    src/test/index.js: simplify code

--- src/test/index.js
+++ src/test/index.js
@@ -1,41 +1,30 @@
 
-const kdo = require('kdo');
 const caller = require('caller');
-
 const config = require('./config');
-const createTestIt = require('./createTestIt');
+
+const createDescribes = require('./createDescribes');
 const request = require('./createTestIt/request');
 
 const data = require('../data');
 const apiInit = require('../api/init');
 
-const flow = {
-	initData({pathToCaller}) {
-		data.initForTest(pathToCaller);
-	},
-
-	initConfig() {
-		config.applyUserConfig();
-	},
-
-	initApiServices() {
-		apiInit(true);
-	},
-
-	initRequest() {
-
-	},
-
-
-
-
-
-	}
-};
-
 /** @name me.test */
 const fn = () => {
-	kdo.sync.do(flow, {pathToCaller: caller()});
+	const pathToCaller = caller();
+
+	data.initForTest(pathToCaller);
+
+	// Load user custom config file and apply it
+	config.applyUserConfig();
+
+	// Load api services for testing
+	apiInit(true);
+
+	// Waiting for server ready when starting test
+	request.init();
+
+	// Create mocha describes
+	createDescribes();
 };
 
 module.exports = fn;
