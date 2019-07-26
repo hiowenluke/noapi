f59df8556e4e40c300f559b18e1728a44ad4ae01
Owen Luke <hi.owen.luke@gmail.com>
2019-07-26 13:55:33 +0800

    move the code of create describe to createDescribe.js

--- src/test/createDescribes/index.js
+++ src/test/createDescribes/index.js
@@ -1,8 +1,7 @@
 
 const config = require('../config');
 const data = require('../../data');
-
-const createTestIt = require('../createTestIt');
+const createDescribe = require('./createDescribe');
 
 const fn = () => {
 	const serviceNames = data.serviceNames;
@@ -10,41 +9,13 @@ const fn = () => {
 	serviceNames.forEach(serviceName => {
 		const title = serviceName === 'default' ? 'api' : serviceName;
 
-		describe(title, () => {
-			const sysName = data.serviceSysNames[serviceName];
-			const defineJs = data.defineJs[sysName];
-			const {api, docs} = defineJs;
+		if (config.enableCatalogs) {
 
-			for (let i = 0; i < api.length; i ++) {
-				const apiInfo = api[i];
-				const docInfos = docs[i];
+		}
+		else {
+		}
 
-				if (!config.isValidTestCase(apiInfo.title)) {
-					continue;
-				}
-
-				if (!docInfos.length) {
-					it('- No test', () => {});
-					continue;
-				}
-
-				docInfos.forEach(docInfo => {
-					const ioInfo = docInfo.io;
-					const testInfo = docInfo.test;
-
-					// No testInfo property and result property, no need to test
-					if (!testInfo && !ioInfo.result) return;
-
-					// No verify property, can not test
-					if (!testInfo.verify) return;
-
-					testInfo.testUrl = testInfo.url || apiInfo.url;
-					testInfo.getResult = testInfo.getResult || apiInfo.url;
-
-					createTestIt(apiInfo, ioInfo, testInfo);
-				});
-			}
-		});
+		createDescribe(title, serviceName);
 	});
 };
 
