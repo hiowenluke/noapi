d5e949e2f90d100a461f042a1c049afe8e2c2861
Owen Luke <hi.owen.luke@gmail.com>
2019-07-26 18:22:24 +0800

    .

---
+++ examples/for-test/03-categorize-the-test-cases-via-config.catalogs/api/define.ok.js
@@ -0,0 +1,175 @@
+
+// See /noapi/src/defineJs_demos.js to learn more.
+
+const me = [
+	{
+		url: 'http://localhost:3000/bill/form/crud?formname=trader',
+		result: {
+			"success": true,
+			"data": {
+				"formname": "trader"
+			}
+		}
+	},
+
+	{
+		url: 'http://localhost:3000/bill/form/getFields?formname=trader',
+		result: {
+			"success": true,
+			"data": [
+				{
+					"fieldname": "billid",
+					"type": "number"
+				},
+				{
+					"fieldname": "billcode",
+					"type": "string"
+				}
+			]
+		}
+	},
+
+	{
+		url: 'http://localhost:3000/bill/list/getData?formname=trader',
+		result: {
+			"success": true,
+			"data": [
+				{
+					"billid": 1,
+					"billcode": "XX-201905-00001"
+				},
+				{
+					"billid": 2,
+					"billcode": "XX-201905-00002"
+				}
+			]
+		}
+	},
+
+	{
+		url: 'http://localhost:3000/bill/list/getFields?formname=trader',
+		result: {
+			"success": true,
+			"data": [
+				{
+					"fieldname": "billid",
+					"type": "number"
+				},
+				{
+					"fieldname": "billcode",
+					"type": "string"
+				}
+			]
+		}
+	},
+
+	{
+		url: 'http://localhost:3000/bill/dropdownlist?formname=paymethod',
+		result: {
+			"success": true,
+			"data": [
+				"Cash",
+				"MasterCard"
+			]
+		}
+	},
+
+	{
+		title: 'Info - Form - Crud // for goods',
+		url: 'http://localhost:3000/info/form/crud?formname=goods',
+		result: {
+			"success": true,
+			"data": {
+				"formname": "goods",
+				"isShowBom": true
+			}
+		}
+	},
+
+	{
+		url: 'http://localhost:3000/info/form/getFields?formname=goods',
+		result: {
+			"success": true,
+			"data": [
+				{
+					"fieldname": "id",
+					"type": "number"
+				},
+				{
+					"fieldname": "code",
+					"type": "string"
+				}
+			]
+		}
+	},
+
+	{
+		title: 'Info - Form - Crud // for employee',
+		url: 'http://localhost:3000/info/form/crud?formname=employee',
+		result: {
+			"success": true,
+			"data": {
+				"formname": "employee",
+				"isShowBom": false
+			}
+		}
+	},
+
+	{
+		url: 'http://localhost:3000/info/list/getData?formname=goods',
+		result: {
+			"success": true,
+			"data": [
+				{
+					"id": 1,
+					"code": "SS-0001"
+				},
+				{
+					"id": 2,
+					"code": "SS-0002"
+				}
+			]
+		}
+	},
+
+	{
+		url: 'http://localhost:3000/info/list/getFields?formname=goods',
+		result: {
+			"success": true,
+			"data": [
+				{
+					"fieldname": "id",
+					"type": "number"
+				},
+				{
+					"fieldname": "code",
+					"type": "string"
+				}
+			]
+		}
+	},
+
+	{
+		url: 'http://localhost:3000/info/dropdownlist?formname=employee',
+		result: {
+			"success": true,
+			"data": [
+				"Captain America",
+				"Iron Man",
+				"Thor"
+			]
+		}
+	},
+
+	{
+		url: 'http://localhost:3000/sys/getApiVersion',
+		result: {
+			"success": true,
+			"data": {
+				"apiVersion": "1.0.0"
+			}
+		}
+	},
+];
+
+module.exports = me;
