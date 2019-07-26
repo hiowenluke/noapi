29287f74f3825aa36ee51315b0218f8e916fe6bf
Owen Luke <hi.owen.luke@gmail.com>
2019-07-26 17:33:44 +0800

    createDescribe.js: add validator {} to check whether the current api path is valid

--- src/test/createDescribes/createDescribe.js
+++ src/test/createDescribes/createDescribe.js
@@ -2,12 +2,56 @@
 const config = require('../config');
 const createTestIt = require('../createTestIt');
 
-const fn = (title, api, docs) => {
+const validator = {
+	isOnlyApiPath: false,
+	regStartWith: null,
+	regExclude: null,
+
+	init(title, isOnlyApiPath, usedApiPaths = []) {
+		this.isOnlyApiPath = isOnlyApiPath;
+		if (!isOnlyApiPath) return;
+
+		if (title) {
+			const startWithApiPath = this.formatRegStr(title);
+			this.regStartWith = new RegExp('^' + startWithApiPath, 'i');
+		}
+
+		if (usedApiPaths.length) {
+			const excludeApiPaths = usedApiPaths.map(usedApiPath => {
+				return this.formatRegStr(usedApiPath);
+			});
+			const excludeApiPathsStr = '(' + excludeApiPaths.join(')|(') + ')';
+			this.regExclude = new RegExp('^' + excludeApiPathsStr, 'i');
+		}
+	},
+
+	formatRegStr(str) {
+		return str
+			.replace(/ /g, '')
+			.replace(/^\S*?(?=\/)/, '')
+			.replace(/\//g, '\\/')
+		;
+	},
+
+	check(apiPath) {
+		if (!this.isOnlyApiPath) return true;
+		return this.regStartWith.test(apiPath) && (!this.regExclude || !this.regExclude.test(apiPath));
+	}
+};
+
+const fn = (title, defineJs, {isOnlyApiPath, usedApiPaths} = {}) => {
+	validator.init(title, isOnlyApiPath, usedApiPaths);
+
+	const {api, docs} = defineJs;
 	describe(title, () => {
 
 		for (let i = 0; i < api.length; i ++) {
 			const apiInfo = api[i];
 			const docInfos = docs[i];
+
+			if (!validator.check(apiInfo.api)) {
+				continue;
+			}
 
 			if (!config.isValidTestCase(apiInfo.title)) {
 				continue;
