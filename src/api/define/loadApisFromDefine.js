9bbaff474dd24a2378ce42af6b5502418590126b
Owen Luke <hi.owen.luke@gmail.com>
2019-08-07 13:49:27 +0800

    loadApisFromDefine.js: check apiInfos first

--- src/api/define/loadApisFromDefine.js
+++ src/api/define/loadApisFromDefine.js
@@ -6,15 +6,15 @@ const lib = require('../../__lib');
 /** @name define.loadApisFromDefine */
 const fn = () => {
 	data.sysNames.forEach(sysName => {
+		const defineJs = data.defineJs[sysName];
+		const apiInfos = defineJs.api;
+		if (!apiInfos || !apiInfos.length) return;
+
 		const core = data.core[sysName];
 
 		// Always initialize the core.api as empty object,
 		// even though it has been loaded from the api directory.
 		core.api = {};
-
-		const defineJs = data.defineJs[sysName];
-		const apiInfos = defineJs.api;
-		if (!apiInfos) return;
 
 		const apiUrls = apiInfos.map(item => item.api);
 		apiUrls.forEach(apiUrl => { // /form:/bill/form/crud
