f614485358da60099fc8ff14bb7ee66ea88206ea
Owen Luke <hi.owen.luke@gmail.com>
2019-08-09 16:32:19 +0800

    .

--- examples/13-call-the-api-which-is-in-other-api-services/api-erp/biz/report/purchase/order.js
+++ examples/13-call-the-api-which-is-in-other-api-services/api-erp/biz/report/purchase/order.js
@@ -1,6 +1,10 @@
 
 const fn = async (query) => {
+
+	// Use global.api.do() to call the api which is in other api services.
+	// See "18-use-noapi.do-instead-of-global.api.do-to-improve-performance" to learn more.
 	const formResult = await global.api.do('forms:/bill/form/crud?formname=trader', query);
+
 	return {
 		billid: query.billid,
 		formname: formResult.formname,
