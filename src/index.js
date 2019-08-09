eb6ae4632189cc590fb9e9084977cb503e525484
Owen Luke <hi.owen.luke@gmail.com>
2019-08-09 16:32:03 +0800

    set noapi.do as the shortcut of data.global.api.do

--- src/index.js
+++ src/index.js
@@ -3,12 +3,19 @@ const caller = require('caller');
 const me = require('kdo')({exclude: 'test'});
 const data = require('./data');
 
+const createNoapiDo = () => {
+	// Add shortcut for data.global.api.do
+	noapi.do = data.global.api.do;
+};
+
 const noapi = (options = {}) => {
 	options.pathToCaller = caller();
 	me.data.init(options);
 
 	me.api.init();
 	me.biz.init();
+
+	createNoapiDo();
 
 	return me.web.start();
 };
