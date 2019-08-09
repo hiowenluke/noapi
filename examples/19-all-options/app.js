035556036ec41e806bcf3087d09ce692cb956f4d
Owen Luke <hi.owen.luke@gmail.com>
2019-08-09 21:43:48 +0800

    add example for all options

---
+++ examples/19-all-options/app.js
@@ -0,0 +1,23 @@
+
+const options = {
+
+	// See terminal logs for more details
+	serverName: 'MyApi',
+	http: 'localhost',
+	port: '9000',
+
+	// See "08-public" for more details
+	public: 'web',
+
+	// See "14-call-the-api-which-is-in-other-api-services-via-assign-rules" for more details
+	assignRules: [],
+
+	// See "16-[for-all-api-services]-preprocess-query-via-power-function" for more details
+	power() {},
+};
+
+// So simple!
+const server = require('../noapi')(options);
+
+// Exports the http server for testing via supertest
+module.exports = server;
