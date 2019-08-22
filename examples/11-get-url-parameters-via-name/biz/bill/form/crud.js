e2dcf89945d02a46d155c06efdd6213ac22efdac
Owen Luke <hi.owen.luke@gmail.com>
2019-08-22 15:26:23 +0800

    add example 11-get-url-parameters-via-name

--- examples/11-get-url-parameters-via-name/biz/bill/form/crud.js
+++ examples/11-get-url-parameters-via-name/biz/bill/form/crud.js
@@ -1,8 +1,12 @@
 
-const fn = async (query) => {
-	const formname = query.formname;
-	const obj = JSON.parse(query.obj);
-	const arr = JSON.parse(query.arr);
+// Note that the parameter name of the function must be the same as the parameter name in the url.
+
+// E.g.:								formname		obj						  arr
+// http://localhost:3000/bill/form/crud?formname=trader&obj={"date":"2019-05-01"}&arr=[1,"abc",{"tel":12345678}]
+
+const fn = async (formname, obj, arr) => {
+	obj = JSON.parse(obj);
+	arr = JSON.parse(arr);
 	return {formname, obj, arr};
 };
 
