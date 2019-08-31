afab192b6e22e7f125a71f946aa8d94175306133
Owen Luke <hi.owen.luke@gmail.com>
2019-08-31 08:33:30 +0800

    .

--- examples/11-get-url-parameters-via-name/api/define.js
+++ examples/11-get-url-parameters-via-name/api/define.js
@@ -20,6 +20,43 @@ const me = [
 			}
 		}
 	},
+
+	{
+		url: 'http://localhost:3000/bill/form/crud',
+
+		params: {
+			formName: "trader",
+
+			obj: {
+				"date": "2019-05-01"
+			},
+
+			arr: [
+				1,
+				"abc",
+				{
+					"tel": 12345678
+				}
+			]
+		},
+
+		result: {
+			"success": true,
+			"data": {
+				"formName": "trader",
+				"obj": {
+					"date": "2019-05-01"
+				},
+				"arr": [
+					1,
+					"abc",
+					{
+						"tel": 12345678
+					}
+				]
+			}
+		}
+	},
 ];
 
 module.exports = me;
--- examples/11-get-url-parameters-via-name/biz/bill/form/crud.js
+++ examples/11-get-url-parameters-via-name/biz/bill/form/crud.js
@@ -1,8 +1,35 @@
 
-// According to the parameters name of the function, take the value from the url parameters.
+// According to the parameters name of the function, take the value
+// from the "url" property or the "params" property in define.js.
 
-// E.g.:								formName		obj						  arr
-// http://localhost:3000/bill/form/crud?formName=trader&obj={"date":"2019-05-01"}&arr=[1,"abc",{"tel":12345678}]
+// 1. From the "url" property:
+// 		{
+// 			...
+// 													   formName		   obj						arr
+// 			url: 'http://localhost:3000/bill/form/crud?formName=trader&obj={"date":"2019-05-01"}&arr=[1,"abc",{"tel":12345678}]'
+// 			...
+// 		}
+
+// 2. Or from the "params" property:
+// 		{
+// 			...
+// 			params: {
+// 				formName: "trader",
+//
+// 				obj: {
+// 					"date": "2019-05-01"
+// 				},
+//
+// 				arr: [
+// 					1,
+// 					"abc",
+// 					{
+// 						"tel": 12345678
+// 					}
+// 				]
+// 			},
+// 			...
+// 	}
 
 const fn = async (formName, obj, arr) => {
 	obj = JSON.parse(obj);
