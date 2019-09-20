0f96146ecfde2fa23378ec075589096d622db6eb
Owen Luke <hi.owen.luke@gmail.com>
2019-09-20 14:00:00 +0800

    .

--- examples/17-all-options/app.js
+++ examples/17-all-options/app.js
@@ -14,6 +14,16 @@ const options = {
 
 	// See "25-[multi]-[for-all-api-services]-preprocess-query-via-power-function" for more details
 	power() {},
+
+	// Options for query object
+	query: {
+
+		// By default, if the value of query parameter is json str, it will be converted to object,
+		// see "10-get-url-parameters-via-query" and "11-get-url-parameters-via-name" for form details.
+
+		// You can close this feature via set the below option as false.
+		isParseJsonStr: false,
+	},
 };
 
 // So simple!
