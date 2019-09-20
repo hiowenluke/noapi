046e06ab2b6eac961a59fa2e588ec531ce66842d
Owen Luke <hi.owen.luke@gmail.com>
2019-09-20 13:48:57 +0800

    .

--- examples/11-get-url-parameters-via-name/biz/bill/form/crud.js
+++ examples/11-get-url-parameters-via-name/biz/bill/form/crud.js
@@ -5,8 +5,8 @@
 // 1. From the "url" property:
 // 		{
 // 			...
-// 													   formName		   obj						arr
-// 			url: 'http://localhost:3000/bill/form/crud?formName=trader&obj={"date":"2019-05-01"}&arr=[1,"abc",{"tel":12345678}]'
+// 													     formName		   obj						   arr
+// 			url: 'http://localhost:3000/bill/form/crud ? formName=trader & obj={"date":"2019-05-01"} & arr=[1,"abc",{"tel":12345678}]'
 // 			...
 // 		}
 
@@ -32,12 +32,6 @@
 // 	}
 
 const fn = async (formName, obj, arr) => {
-
-	// If the request is a "get" action, then the obj is a string.
-	// If the request is a "post" action, then the obj is an object.
-	if (typeof obj === 'string') obj = JSON.parse(obj);
-	if (typeof arr === 'string') arr = JSON.parse(arr);
-
 	return {formName, obj, arr};
 };
 
