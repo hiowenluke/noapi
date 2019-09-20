809c33d446d8bb6ef43e0a998898b55bbd1eb44b
Owen Luke <hi.owen.luke@gmail.com>
2019-09-20 14:00:04 +0800

    .

--- src/biz/do.js
+++ src/biz/do.js
@@ -2,6 +2,16 @@
 const data = require('../data');
 const lib = require('../__lib');
 
+const tryParseJSON = (jsonString) => {
+	try {
+		const o = JSON.parse(jsonString);
+		if (o && typeof o === "object") {
+			return o;
+		}
+	}
+	catch (e) {}
+};
+
 // Convenient api quick calls to its biz method
 // 		complete: 		data.core.mms.biz.bill.mnf.manuPlan(query)
 // 		Shorthand: 		global.biz.do(query)
@@ -23,6 +33,16 @@ const fn = async (query) => {
 	const sysBizFn = lib.getSysApiFn(sysName, api, sysBizs);
 	if (typeof sysBizFn !== 'function') {
 		return {error: `The biz function corresponding to api '${api}' does not exists.`};
+	}
+
+	// Automatically parse json string if needed
+	if (data.queryOptions.isParseJsonStr) {
+		Object.keys(query).forEach(key => {
+			const o = tryParseJSON(query[key]);
+			if (o) {
+				query[key] = o;
+			}
+		});
 	}
 
 	// If there is no params, or just only one parameter named "query", pass the whole query
