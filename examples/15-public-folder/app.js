f01ad0d31e8d427fbda86e05914b4cc0c2ba87d9
Owen Luke <hi.owen.luke@gmail.com>
2019-09-25 17:28:35 +0800

    .

--- examples/15-public-folder/app.js
+++ examples/15-public-folder/app.js
@@ -1,8 +1,9 @@
 
-// If there is a folder "./public", noapi will automatically load it as express static resources,
-// and the url corresponds to it is http://localhost:3000/public .
+// If there is a folder "public" under the root of project, it will be loaded as a express static resource.
+//		1. If a path was specified as "xxx", the url corresponds to it is http://localhost:3000/xxx .
+//		2. If no path was specified, the url corresponds to it is http://localhost:3000 .
 
-// You can specify another folder such as "./web" or another name "http://localhost:3000/xxx" to replace it:
+// You can specify another folder such as "web" to replace it.
 // See "16-set-public-folder" to learn more.
 
 // So simple!
--- examples/16-set-public-folder/app.js
+++ examples/16-set-public-folder/app.js
@@ -1,12 +1,13 @@
 
-// If there is a folder "./public", noapi will automatically load it as express static resources,
-// and the url corresponds to it is http://localhost:3000/public .
+// If there is a folder "public" under the root of project, it will be loaded as a express static resource.
+//		1. If a path was specified as "xxx", the url corresponds to it is http://localhost:3000/xxx .
+//		2. If no path was specified, the url corresponds to it is http://localhost:3000 .
 
-// You can specify another folder such as "./web" or another name "http://localhost:3000/xxx" to replace it.
+// You can specify another folder such as "web" to replace it.
 // Try each of options below to learn more.
 
 const options1 = {
-	public: './web' // <- http://localhost:3000/public
+	public: 'web' // <- http://localhost:3000
 };
 
 // Equivalent to options1
@@ -14,30 +15,36 @@ const options2 = {
 	public: {
 
 		// Specify the public folder
-		path: './web' // <- http://localhost:3000/public
+		folder: 'web' // <- http://localhost:3000
 	}
 };
 
+// Full public options
 const options3 = {
 	public: {
 
-		// specify the public name
-		name: 'xxx', // <- http://localhost:3000/xxx
-		path: './web',
+		// Specify the public folder
+		folder: 'web',
+
+		// Specify the public path
+		path: 'public', // <- http://localhost:3000/public
 	}
 };
 
+// Full public options
 const options4 = {
 	public: {
 
-		// specify the public name
-		name: '/', // <- http://localhost:3000/
-		path: './web',
+		// Specify the public folder
+		folder: 'web',
+
+		// Specify the public path
+		path: 'xxx', // <- http://localhost:3000/xxx
 	}
 };
 
 // So simple!
-const server = require('../noapi')(options3);
+const server = require('../noapi')(options4);
 
 // Exports the http server for testing via supertest
 module.exports = server;
