94704aaa1319d667cb09fcea00e46dcb6a50ecb8
Owen Luke <hi.owen.luke@gmail.com>
2019-10-22 01:13:25 +0800

    Supplement comments to examples

--- examples/01-define-API-and-test-cases-with-array/app.js
+++ examples/01-define-API-and-test-cases-with-array/app.js
@@ -9,14 +9,3 @@ module.exports = server;
 // 		The server is an instance of http.server.
 // 		See below documentations to learn more:
 //		https://nodejs.org/api/http.html#http_class_http_server
-
-// Further more, we can get app, server, express like below:
-// 		const {app, server, express} = require('../noapi')();
-
-// 		The app is an instance of express, "app = express()".
-//		The server is an instance of http.server, "server = http.createServer(...)".
-//		The express is expressJs itself.
-
-//		See below examples to learn more:
-//		./examples/13-use-express-middleware/app.js
-//		./examples/14-custom-express-route/app.js
