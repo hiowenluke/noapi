10817aa36f9f839acdf679b9eea3fd8c1a86c75f
Owen Luke <hi.owen.luke@gmail.com>
2019-10-23 15:05:56 +0800

    Update manual tests

--- test/manual-tests/01-no-api-folder/app.js
+++ test/manual-tests/01-no-api-folder/app.js
@@ -1,13 +1,16 @@
 
-const server = require('../../../src')();
+const options = {
+	prompt: `
+		-------------------------------------------
+		The api and biz folder will be automatically created if they are not exist.
+		Please visit http://localhost:3000/bill/form/crud, then the server should returns below:
+		{
+			"success": false, 
+			"error": "No api corresponds to url /bill/form/crud"
+		}
+		-------------------------------------------
+	`,
+};
 
-console.log(`
-	The api and biz folder will be automatically created if they are not exist.
-	Please visit http://localhost:3000/bill/form/crud, then the server should returns below:
-	{
-		"success": false, 
-		"error": "No api corresponds to url /bill/form/crud"
-	}
-`.replace(/\n\t/g, '\n').replace(/\t/g, ' '.repeat(4)));
-
+const server = require('../../../src')(options);
 module.exports = server;
--- test/manual-tests/02-no-biz-folder/app.js
+++ test/manual-tests/02-no-biz-folder/app.js
@@ -1,13 +1,16 @@
 
-const server = require('../../../src')();
+const options = {
+	prompt: `
+		-------------------------------------------
+		The biz folder will be automatically created if it is not exists.
+		Please visit http://localhost:3000/hello, then the server should returns below:
+		{
+			"success": false,
+			"error": "The handler ./biz/hello.js does not exists."
+		}
+		-------------------------------------------
+	`,
+};
 
-console.log(`
-	The biz folder will be automatically created if it is not exists.
-	Please visit http://localhost:3000/hello, then the server should returns below:
-	{
-		"success": false,
-		"error": "The handler ./biz/hello.js does not exists."
-	}
-`.replace(/\n\t/g, '\n').replace(/\t/g, ' '.repeat(4)));
-
+const server = require('../../../src')(options);
 module.exports = server;
