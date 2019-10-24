0f1e68a767d096e1a0843009632878344695c6c9
Owen Luke <hi.owen.luke@gmail.com>
2019-10-24 15:29:58 +0800

    Update template ./biz/do/say/hi.js

--- src/__template/biz/do/say/hi.js
+++ src/__template/biz/do/say/hi.js
@@ -5,13 +5,17 @@
 const fn = async (query) => {
 	return {
 		msg: `Hi, I'm ${query.name}, ${query.age} years old.`,
+		name: query.name,
+		age: query.age - 0, // convert it to number
 	};
 
     // Output to client:
 	// 		{
 	// 			"success": true,
 	// 			"data": {
-	// 				"msg": "Hi, I'm Owen, 100 years old."
+	//				"msg": "Hi, I'm Owen, 100 years old.",
+	// 				"name": "Owen",
+	//				"age": 100
 	// 			}
 	// 		}
 };
