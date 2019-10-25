39227e49d09be96a734ba49997c998e01c654e68
Owen Luke <hi.owen.luke@gmail.com>
2019-10-25 14:42:11 +0800

    Update special tests #1

--- test/special/01-no-api-folder-[clone-from-template]/app.js
+++
@@ -1,6 +0,0 @@
-
-// In this test case, there are not api and biz directory.
-// Noapi will clone them from template and print welcome messages in terminal.
-
-const server = require('../noapi')();
-module.exports = server;
