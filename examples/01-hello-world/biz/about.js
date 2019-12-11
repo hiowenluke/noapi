9b6058b56be10c607c96426b05bf0b6e2b5dcbd7
Owen Luke <hi.owen.luke@gmail.com>
2019-12-11 10:27:00 +0800

    update examples #1

--- examples/01-hello-world/biz/about.js
+++ examples/01-hello-world/biz/about.js
@@ -1,4 +1,13 @@
 
+// Test url
+// 		http://localhost:3000/about
+
+// Expected result
+//		{
+// 			"success": true,
+// 			"data": "Author: Owen Luke"
+// 		}
+
 const fn = async (query) => {
 	return `Author: Owen Luke`;
 };
--- examples/01-hello-world/biz/say/hi.js
+++ examples/01-hello-world/biz/say/hi.js
@@ -1,4 +1,15 @@
 
+// Test url
+// 		http://localhost:3000/say/hi?name=owen&age=100
+
+// Expected result
+// 		{
+// 			"success": true,
+// 			"data": {
+// 				"msg": "Hi, I'm owen, 100 years old."
+// 			}
+// 		}
+
 const fn = async (query) => {
 	return {msg: `Hi, I'm ${query.name}, ${query.age} years old.`};
 };
