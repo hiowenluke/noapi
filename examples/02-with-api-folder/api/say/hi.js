b30cb96e944993c1f327f66b4a2156f5bdd1ff79
Owen Luke <hi.owen.luke@gmail.com>
2019-12-11 10:49:13 +0800

    Update hi.js

--- examples/02-with-api-folder/api/say/hi.js
+++ examples/02-with-api-folder/api/say/hi.js
@@ -1,18 +1,15 @@
-/*
-	Test url
-		http://localhost:3000/say/hi?name=owen&age=100
+/**
+ * @test
+ 	http://localhost:3000/say/hi?name=owen&age=100
 
-	Expected results
-		{
-			"success": true, // The state of the result
-			"data": {        // The data of the result
-				"msg": "Hi, I'm owen, 100 years old."
-			}
+ * @returns
+ 	{
+		"success": true,
+		"data": {
+			"msg": "Hi, I'm owen, 100 years old."
 		}
+	}
 
-	Arguments
-		name 		user name, must be a string with a maximum length of 32
-
-		age			user age, must be a number with a maximum of 200
-
-*/
+ * @param {String} 	name 	user's name with a maximum length of 32
+ * @param {Integer} age		user's age with a maximum of 200
+ * */
