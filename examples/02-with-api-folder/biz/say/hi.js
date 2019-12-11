4d6200c1eab8401390d5f3b4a20aa14c8591c108
Owen Luke <hi.owen.luke@gmail.com>
2019-12-11 10:01:39 +0800

    Update hi.js

--- examples/02-with-api-folder/biz/say/hi.js
+++ examples/02-with-api-folder/biz/say/hi.js
@@ -1,6 +1,13 @@
 
+const reverse = require('../__lib/reverse');
+const encode = require('./__lib/encode');
+
 const fn = async (query) => {
-	return {msg: `Hi, I'm ${query.name}, ${query.age} years old.`};
+	const msg = `Hi, I'm ${query.name}, ${query.age} years old.`;
+	const reversed = reverse(msg);
+	const encoded = encode(msg);
+
+	return {msg, reversed, encoded};
 };
 
 module.exports = fn;
