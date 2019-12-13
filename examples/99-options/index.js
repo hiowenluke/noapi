56098bf31d420b0d56582e458e20b9ffa41918c3
Owen Luke <hi.owen.luke@gmail.com>
2019-12-13 18:27:36 +0800

    update examples

--- examples/99-options/index.js
+++ examples/99-options/index.js
@@ -1,7 +1,9 @@
 
-const options = {
-	name: 'myApi',
-	port: 3001,
-};
+const noapi = require('..');
 
-require('../noapi')(options);
+const name = 'myApi'; // the default value is "default"
+const port = 3001; // the default value is 3000
+const isSilence = true; // the default value is false
+
+// The order of the parameters can be arbitrary
+noapi(name, port, isSilence);
