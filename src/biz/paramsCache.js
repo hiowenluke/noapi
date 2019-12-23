66288a13ea4d7f45fc50ecc6ba9f865eda5acdd8
Owen Luke <hi.owen.luke@gmail.com>
2019-12-23 09:49:49 +0800

    Update paramsCache.js

--- src/biz/paramsCache.js
+++ src/biz/paramsCache.js
@@ -18,10 +18,11 @@ const me = {
 
 	init() {
 		const root = config.webServiceRoot;
+		const bizFolder = config.folder.replace(/^./, ''); // ./biz => /biz
 		const apis = data.apis;
 
 		apis.forEach(api => {
-			const filePath = root + '/biz' + api + '.js';
+			const filePath = root + bizFolder + api + '.js';
 			if (!fs.existsSync(filePath)) return;
 
 			const bizFile = require(filePath);
