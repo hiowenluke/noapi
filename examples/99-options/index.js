40a0f0724957b74db5cc6a07c58b708b7ec035f0
Owen Luke <hi.owen.luke@gmail.com>
2019-12-23 10:14:05 +0800

    Update index.js

--- examples/99-options/index.js
+++ examples/99-options/index.js
@@ -2,18 +2,18 @@
 const noapi = require('../noapi');
 
 const name = 'myApi'; // default is "default"
-const directory = './src'; // default is "./biz"
+const dir = './src'; // default is "./biz"
 const host = '127.0.0.1'; // default is "localhost"
 const port = 3001; // default is 3000
 const isSilence = true; // default is false
 
 // The number and order of parameters can be arbitrary
-noapi(name, directory, host, port, isSilence);
+noapi(name, dir, host, port, isSilence);
 
 // Equiv to:
 // 		const options = {
 // 			name: 'myApi',
-// 			directory: './src',
+// 			dir: './src',
 // 			host: '127.0.0.1',
 // 			port: 3001,
 // 			isSilence: true,
