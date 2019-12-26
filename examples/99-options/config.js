15a5032477b95f9233be8dc8b365fce048e96d8d
Owen Luke <hi.owen.luke@gmail.com>
2019-12-26 18:45:15 +0800

    update exampels #99

---
+++ examples/99-options/config.js
@@ -0,0 +1,8 @@
+
+module.exports = {
+	name: 'myApi',
+	dir: './src',
+	host: '127.0.0.1',
+	port: 3001,
+	isSilence: true,
+};
--- examples/99-options/index.js
+++ examples/99-options/index.js
@@ -1,22 +1,5 @@
 
 const noapi = require('../noapi');
+const config = require('./config');
 
-const name = 'myApi';
-const dir = './src';
-const host = '127.0.0.1';
-const port = 3001;
-const isSilence = true;
-
-// The number and order of parameters can be arbitrary
-noapi(name, dir, host, port, isSilence);
-
-// It is equivalents to:
-// 		const options = {
-// 			name: 'myApi',
-// 			dir: './src',
-// 			host: '127.0.0.1',
-// 			port: 3001,
-// 			isSilence: true,
-// 		};
-//
-// 		noapi(options);
+noapi(config);
