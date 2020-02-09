4c7be3c61c4eb1f7147f281daa123bcd1996b08b
Owen Luke <hi.owen.luke@gmail.com>
2020-02-09 23:03:32 +0800

    Create cross.js

---
+++ src/server/cross.js
@@ -0,0 +1,18 @@
+
+const fn = (res) => {
+
+	// Website you wish to allow to connect
+	res.setHeader('Access-Control-Allow-Origin', '*');
+
+	// Request methods you wish to allow
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+	// Request headers you wish to allow
+	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+	// Set to true if you need the website to include cookies in the requests sent
+	// to the API (e.g. in case you use sessions)
+	res.setHeader('Access-Control-Allow-Credentials', true);
+};
+
+module.exports = fn;
